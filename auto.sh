e47bebd05b91a863a0d45cc54f99bcf76aa6cc3c
sarfrazcd <71914039+sarfrazcd@users.noreply.github.com>
2021-03-21 12:28:43 +0500

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,50 +1,52 @@
-if command -v python2 >/dev/null 2>&1; then
- echo 'python2 already installed'
-else
-pkg install python2 -y
-fi
-
-
-if command -v python3 >/dev/null 2>&1; then
- echo 'python3 already installed'
-else
-pkg install python3 -y
-fi
-
-
-if command -v git >/dev/null 2>&1; then
- echo 'git already installed'
-else
-pkg install git -y
-fi
-
 if pip show requests | grep Version >/dev/null 2>&1; then
  echo 'requests already installed'
 else
-pip install requests -y
-pip2 install requests -y
+pip install requests 
 fi
 
 if pip show mechanize | grep Version >/dev/null 2>&1; then
  echo 'mechanize already installed'
 else
-pip install mechanize -y
-pip2 install mechanize -y
+pip install mechanize
 fi
 
 
-if command -v lolcat >/dev/null 2>&1; then
+if pip show lolcat | grep Version >/dev/null 2>&1; then
  echo 'lolcat already installed'
 else
-pip install lolcat -y
-pip2 install lolcat -y
+pip install lolcat
 fi
 
 if pip show bs4 | grep Version >/dev/null 2>&1; then
  echo 'bs4 already installed'
 else
-pip install bs4 -y
-pip2 install bs4 -y
+pip install bs4
+fi
+
+
+if pip2 show requests | grep Version >/dev/null 2>&1; then
+ echo 'requests already installed'
+else
+pip2 install requests
+fi
+
+if pip2 show mechanize | grep Version >/dev/null 2>&1; then
+ echo 'mechanize already installed'
+else
+pip2 install mechanize 
+fi
+
+
+if pip2 show lolcat | grep Version>/dev/null 2>&1; then
+ echo 'lolcat already installed'
+else
+pip2 install lolcat
+fi
+
+if pip2 show bs4 | grep Version >/dev/null 2>&1; then
+ echo 'bs4 already installed'
+else
+pip2 install bs4
 fi
 
 
