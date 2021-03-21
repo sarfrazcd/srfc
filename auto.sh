d73eec5fe9c550925d09403a124005a994e575f5
sarfrazcd <71914039+sarfrazcd@users.noreply.github.com>
2021-03-21 12:04:31 +0500

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,20 +1,53 @@
 if command -v python2 >/dev/null 2>&1; then
  echo 'python2 already installed'
 else
-pkg install python2
+pkg install python2 -y
 fi
+
+
 if command -v python3 >/dev/null 2>&1; then
  echo 'python3 already installed'
 else
-pkg install python3
+pkg install python3 -y
 fi
+
+
 if command -v git >/dev/null 2>&1; then
  echo 'git already installed'
 else
-pkg install git
+pkg install git -y
 fi
-pip2 install requests mechanize lolcat bs4
-pip2 install requests mechanize lolcat bs4
+
+if command -v requests >/dev/null 2>&1; then
+ echo 'requests already installed'
+else
+pip install requests
+pip2 install requests
+fi
+
+if command -v mechanize >/dev/null 2>&1; then
+ echo 'mechanize already installed'
+else
+pip install mechanize
+pip2 install mechanize
+fi
+
+
+if command -v lolcat >/dev/null 2>&1; then
+ echo 'lolcat already installed'
+else
+pip install lolcat
+pip2 install lolcat
+fi
+
+if command -v bs4 >/dev/null 2>&1; then
+ echo 'bs4 already installed'
+else
+pip install bs4
+pip2 install bs4
+fi
+
+
 cd ~
 termux-setup-storage
 cp -r srfb /data/data/com.termux/files/home/storage/music
